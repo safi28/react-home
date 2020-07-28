382c45a0601bd839b8150f6b3a45835a938f9c38
Saphie <safihanadi28@gmail.com>
2020-07-28 18:02:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App/App";
+import Navigation from "./Navigation";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <App>
+      <Navigation />
+    </App>
   </BrowserRouter>,
   document.getElementById("root")
 );
