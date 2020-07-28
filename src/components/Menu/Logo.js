dbeb0cd2c97e6ddc18495bf19cbbd423ba78dcac
Saphie <safihanadi28@gmail.com>
2020-07-28 18:00:58 +0300

    moved to components folder

---
+++ src/components/Menu/Logo.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../../styles/logo.module.css";
+import UserContext from "../../ContextWrapper/User";
+
+const Logo = () => {
+  const contextValue = React.useContext(UserContext);
+  console.log(contextValue);
+  const { username } = contextValue;
+  // const { username } = user
+  console.log(contextValue);
+  return (
+    <div className={styles["vertical-menu-logo"]}>
+      <div>
+        <img
+          className={styles.image}
+          src={require("../../static/images/home-run.png")}
+          alt="img"
+        />{" "}
+        <span>Hi, {username}</span>
+      </div>
+      <span className={styles["open-menu-btn"]}>
+        <hr></hr>
+        <hr></hr>
+        <hr />
+      </span>
+    </div>
+  );
+};
+export default Logo;
