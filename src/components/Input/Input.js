8350fc71dbc39f6db785456ab44291b41cc4ad00
Saphie <safihanadi28@gmail.com>
2020-07-28 18:00:18 +0300

    Create Input.js

---
+++ src/components/Input/Input.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./input.module.css";
+
+const Input = (props) => {
+  return (
+    <>
+      <input
+        type = {props.type}
+        className={styles["form-field"]}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        disabled={props.isDisabled}
+      />
+      {/* {props.error && <div className="error">{props.error}</div>} */}
+    </>
+  );
+};
+export default Input;
